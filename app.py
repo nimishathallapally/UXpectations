e110dc5dd63f3b0e60793040fc789e5e40f71da9
Nimisha Thallapally <nimishathallapally@gmail.com>
2025-02-06 14:25:53 +0530

    Added Landing Page

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ def home():
     # Render the homepage with good and bad design images
     return render_template('index.html')
 
+@app.route('/good-design')  # Updated URL path for Good Design
+def good_design():
+    return render_template('design.html')  # Keep current page
+
+@app.route('/bad-design')  # Updated URL path for Bad Design
+def bad_design():
+    return render_template('design.html')  # Keep current page
+
 if __name__ == "__main__":
     app.run(debug=True)
