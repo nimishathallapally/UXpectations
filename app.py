c9917bffa51959b628bcd0820b2d699af5207c7c
Nimisha Thallapally <nimishathallapally@gmail.com>
2025-02-10 10:16:43 +0530

    Modify app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/good-design')  # Updated URL path for Good Design
 def good_design():
-    return render_template('design.html')  # Keep current page
+    return render_template('home.html')  # Keep current page
 
 @app.route('/bad-design')  # Updated URL path for Bad Design
 def bad_design():
