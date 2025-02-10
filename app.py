7b78191c190050b5127fa03adfcf0da6e5a86cec
ishankkumar-007 <ishankkumar123@gmail.com>
2025-02-10 21:37:21 +0530

    feat: signup and login completed, fixed the routes to other pages

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/good-home')
 def good_home():
@@ -22,16 +24,26 @@ def bad_design():
     return render_template('bad/design_bad.html')
 
 
-@app.route('/login')
-def login():
+@app.route('/bad-login')
+def bad_login():
+    return render_template('bad/login_bad.html')
+@app.route('/bad-signup')
+def bad_signup():
+    return render_template('bad/signup_bad.html')
+
+
+@app.route('/good-login')
+def good_login():
     return render_template('good/login.html')
-@app.route('/signup')
-def signup():
+@app.route('/good-signup')
+def good_signup():
     return render_template('good/signup.html')
+
 
 @app.route('/quiz')
 def take_quiz():
     return render_template('good/quiz.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
